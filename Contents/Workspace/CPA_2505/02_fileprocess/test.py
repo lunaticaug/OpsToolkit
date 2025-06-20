2fe0659e2acf9d0bc0a68223eba7d02b896d834a
Windows11 <certaintyequivalent@test.com>
2025-06-20 18:53:17 +0900

    delete

--- Contents/Workspace/CPA_2505/02_fileprocess/test.py
+++
@@ -1,6 +0,0 @@
-# list_word_fileconverters.py
-import win32com.client as win32
-word = win32.Dispatch("Word.Application")
-for fc in word.FileConverters:
-    print(fc.Id, fc.FormatName)
-word.Quit()
